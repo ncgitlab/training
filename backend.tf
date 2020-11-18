35c258ba70e5ebd9b3431e4fc3caeeb58c96d3d1
Ncgitlab <49904433+Ncgitlab@users.noreply.github.com>
2020-11-18 18:23:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket = "awstesing-2020"
     key    = "terraformprd.tfstate"
     region = "us-east-1"
-    secret_key =  "jhBvnhGj6iAwKQmP2g207lryHsDKKkyiZ3Llo200"
-    access_key = "AKIA2SS2LBWLJDWDB6X5"
+    secret_key =  ""
+    access_key = ""
     dynamodb_table = "s3-backend-locking"
   }
 }
